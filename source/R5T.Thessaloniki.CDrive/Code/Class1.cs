bfc594f5e7118260897ef27ea14868201627636c
MinexAutomation <minex.automation@gmail.com>
2020-01-22 17:20:10 -0800

    Added IServiceCollection extensions.

--- source/R5T.Thessaloniki.CDrive/Code/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace R5T.Thessaloniki.CDrive
-{
-    public class Class1
-    {
-    }
-}
---
+++ source/R5T.Thessaloniki.CDrive/Code/Services/Extensions/IServiceCollectionExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+using Microsoft.Extensions.DependencyInjection;
+
+using R5T.Copenhagen.Standard;
+using R5T.Lombardy.Standard;
+using R5T.Thessaloniki.Copenhagen;
+using R5T.Troy.Standard;
+
+
+namespace R5T.Thessaloniki.CDrive
+{
+    public static class IServiceCollectionExtensions
+    {
+        public static IServiceCollection AddTemporaryDirectoryPathProvider(this IServiceCollection services)
+        {
+            services
+                .AddSingleton<ITemporaryDirectoryPathProvider, CDriveTemporaryDirectoryPathProvider>()
+                .AddCDrivePathProvider()
+                .AddTemporaryDirectoryNameConvention()
+                .AddStringlyTypedPathOperator()
+                ;
+
+            return services;
+        }
+    }
+}
