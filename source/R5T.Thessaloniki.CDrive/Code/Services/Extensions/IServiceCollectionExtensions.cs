8ae2c41dbea0c2cfbc827f2e3742f740fea57601
MinexAutomation <minex.automation@gmail.com>
2020-01-30 01:51:19 -0800

    Updated IServiceCollections to use ServiceAction<T> functionality.

--- source/R5T.Thessaloniki.CDrive/Code/Services/Extensions/IServiceCollectionExtensions.cs
+++ source/R5T.Thessaloniki.CDrive/Code/Services/Extensions/IServiceCollectionExtensions.cs
@@ -3,6 +3,7 @@
 using Microsoft.Extensions.DependencyInjection;
 
 using R5T.Copenhagen.Standard;
+using R5T.Dacia;
 using R5T.Lombardy.Standard;
 using R5T.Thessaloniki.Copenhagen;
 using R5T.Troy.Standard;
@@ -12,16 +13,27 @@ namespace R5T.Thessaloniki.CDrive
 {
     public static class IServiceCollectionExtensions
     {
+        /// <summary>
+        /// Adds a <see cref="ITemporaryDirectoryPathProvider"/> service.
+        /// </summary>
         public static IServiceCollection AddTemporaryDirectoryPathProvider(this IServiceCollection services)
         {
-            services
-                .AddSingleton<ITemporaryDirectoryPathProvider, CDriveTemporaryDirectoryPathProvider>()
-                .AddCDrivePathProvider()
-                .AddTemporaryDirectoryNameConvention()
-                .AddStringlyTypedPathOperator()
+            services.AddCDriveTemporaryDirectoryPathProvider(
+                services.AddCDrivePathProviderAction(),
+                services.AddTemporaryDirectoryNameConventionAction(),
+                services.AddStringlyTypedPathOperatorAction())
                 ;
 
             return services;
+        }
+
+        /// <summary>
+        /// Adds a <see cref="ITemporaryDirectoryPathProvider"/> service.
+        /// </summary>
+        public static ServiceAction<ITemporaryDirectoryPathProvider> AddTemporaryDirectoryPathProviderAction(this IServiceCollection services)
+        {
+            var serviceAction = new ServiceAction<ITemporaryDirectoryPathProvider>(() => services.AddTemporaryDirectoryPathProvider());
+            return serviceAction;
         }
     }
 }
